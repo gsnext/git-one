57e693d85bae74d562e4d649b1cbe4624635c598
gsnexxt <gsnexxt@gmail.com>
2019-08-17 14:42:48 -0700

    added comment to analysis_numerical.py

--- Diabetes-Patients-Readmission-Prediction-master/analysis_numerical.py
+++ Diabetes-Patients-Readmission-Prediction-master/analysis_numerical.py
@@ -5,7 +5,7 @@ import scipy.io as sio
 import scipy.sparse as sp
 from scipy.sparse import csc_matrix
 from sklearn import preprocessing
-
+#hello
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm
@@ -72,12 +72,12 @@ def KNearest_Neighbour_Weighted_Similarity(X_train, y_train, X_test, y_test, num
 	return neigh.score(X= X_test, y=y_test)
 
 def DistanceArrayToSimilarityArray(arr):
-	# print "Distance Array input is:\n", csc_matrix(arr) 
+	# print "Distance Array input is:\n", csc_matrix(arr)
 	global sigma
 	# print "sigma in DistanceArrayToSimilarityArray function is:", sigma
 	if(sigma == None):
 		raise ValueError("Global sigma is not set!")
-	K = np.exp(-1 * arr ** 2 / (2* float(sigma) ** 2)) # the larger the sigma, the closer K is to 1 
+	K = np.exp(-1 * arr ** 2 / (2* float(sigma) ** 2)) # the larger the sigma, the closer K is to 1
 	# print K
 	return K # k is of shape (N_sampels, N_samples)
 
@@ -140,7 +140,7 @@ def SVM_Analysis (data, label_column, num_folds, SVMToUse, l1_penalty = 1.0):
 	return np.mean(accuracies)
 
 def featureSelection_Linear_SVM (data, label, penalty): # data in shape(N_samples, N_feature), label is a 1D vector (N_samples,)
-	svc_svm = svm.LinearSVC(penalty = 'l1', dual= False, C = penalty) # Tune the C for sparsity 
+	svc_svm = svm.LinearSVC(penalty = 'l1', dual= False, C = penalty) # Tune the C for sparsity
 	svc_svm.fit(data, label)
 	selected_data = svc_svm.transform(data)
 	# print "The selected shape is:", selected_data.shape
@@ -155,7 +155,7 @@ def featureSelection_Correlation(data, label): # data is in (N_samples, N_featur
 	print label_features_r**2
 	print "Correlation square in decreasing sequence:\n", np.sort(label_features_r**2)[::-1]
 	return np.argsort(label_features_r**2)[::-1]
-	
+
 def featureSelection_SingleVariablePerformance(data, label, analysis): # analysis could be string of 'KNN', 'SVM_SVC' or 'SVM_Linear', 5 fold default, 7 neighbors default for Knn
 	rank = np.zeros(data.shape[1])
 	for i in range (0, data.shape[1]):
@@ -183,7 +183,7 @@ def ensembleClassificationWeightedVotes(data, label_column, num_folds, num_neigh
 	kf = StratifiedKFold(label_column, n_folds=num_folds) # need stratified KF, otherwise, the normal KF just picks the first 80 as a subsample, 80-160 as another subsample -> in this case, the test data's label info won't be even in the training data set
 	for train, test in kf:
 		X_train, X_test, y_train, y_test = data[train], data[test], label_column[train], label_column[test]
-		
+
 		knn = KNeighborsClassifier(n_neighbors=num_neighbours) # n neighbours = 1,3,5, default distance is standard Euclidean distance, weights in prediction is uniform
 		knn.fit(X=X_train, y=y_train)
 		knn_label = knn.predict(X_test)
@@ -213,7 +213,7 @@ def ensembleClassificationWeightedVotes(data, label_column, num_folds, num_neigh
 
 
 		accuracy = accuracy_score(y_test, weightedVotesLabel(flag, knn_label, lin_svm_label, svc_svm_label, dtc_label))
-		print "Ensemble accuracy for one run in 5-fold:",accuracy 
+		print "Ensemble accuracy for one run in 5-fold:",accuracy
 		accuracies.append(accuracy)
 	global sigma
 	print "The mean of the accuracies:", np.mean(accuracies), "; The standard Deviation of the accuracies:", np.std(accuracies, dtype=np.float64), "" if (sigma == None) else "with sigma tuned as {sigma}".format(sigma= sigma)
@@ -233,7 +233,7 @@ def weightedVotesLabel(isForBagOfDrug, knn_label, lin_svm_label, svc_svm_label,d
 		DECISION_WEIGHT = 0.556
 
 	WEIGHT_SUM = KNN_WEIGHT + LIN_SVM_WEIGHT + SVC_SVM_WEIGHT+ DECISION_WEIGHT
-	
+
 	weighted_probability = {
 	0:KNN_WEIGHT/WEIGHT_SUM,
 	1:LIN_SVM_WEIGHT/WEIGHT_SUM,
@@ -242,7 +242,7 @@ def weightedVotesLabel(isForBagOfDrug, knn_label, lin_svm_label, svc_svm_label,d
 	}
 
 	print np.argsort([weighted_probability[0], weighted_probability[1], weighted_probability[2], weighted_probability[3]])[::-1]
-	
+
 	final_label = np.zeros(len(knn_label), dtype = int)
 	label_pool = np.vstack((knn_label,lin_svm_label,svc_svm_label,dtc_label))
 	for j in range(0, label_pool.shape[1]):
@@ -272,7 +272,7 @@ def decision_tree_analysis(data, label_column, num_folds ):
 		accuracies.append(accuracy)
 	global sigma
 	print "The mean of the accuracies:", np.mean(accuracies), "; The standard Deviation of the accuracies:", np.std(accuracies, dtype=np.float64), "" if (sigma == None) else "with sigma tuned as {sigma}".format(sigma= sigma)
-	return np.mean(accuracies)	
+	return np.mean(accuracies)
 
 def main():
 	# data_numerical = loadArray("data_numerical.npz")
@@ -418,7 +418,7 @@ def main():
 		# print "Number of features in the set:", len(feature_weights_numerical[feature_weights_numerical!=0])
 		print "feature set is:", np.array(range(0,8))[feature_weights_numerical[0]!=0]
 		feature_sets.append(len(feature_weights_numerical[feature_weights_numerical!=0]))
-	
+
 
 		#RFE is the same as ranking the features based on the absolute value of coef_ given, might not be as good as tuning C in the LinearSVC model
 		#estimator = svm.LinearSVC(penalty = 'l1', dual = False, C = numerical_data_penalty)
@@ -445,7 +445,7 @@ def main():
 		# plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
 		# plt.savefig("feature_selection/RFECV_numerical_{n}_featuresSet_C_{c}.png".format(n= len(feature_weights_numerical[feature_weights_numerical!=0]), c = numerical_data_penalty))
 		# plt.show()
-	
+
 	print "last entry in optimal scores",optimal_scores[len(optimal_scores)-1]
 	plt.plot(feature_sets, optimal_scores)
 	# plt.plot(feature_sets, average_scores)
@@ -454,7 +454,7 @@ def main():
 	plt.savefig("feature_selection/numerical_age_quantized_optimal_accuracy_VS_num_featureSet.png")
 	plt.clf()
 	# plt.show()
-	
+
 
 
     #---------------l1 SVM non zero coefficients on Bag of Drugs--------------
@@ -486,7 +486,7 @@ def main():
 		# print "average accuracy with this features set:", average_accuracy
 		# average_scores.append(average_accuracy)
 
-		#RFECV tuned the number of features to include by cross validation, however, feature importance are still based on the absolute value of coef_; 
+		#RFECV tuned the number of features to include by cross validation, however, feature importance are still based on the absolute value of coef_;
 		lin_svm = svm.LinearSVC(penalty = 'l1', dual= False, C = bag_drugs_penalty)
 		rfecv = fs.RFECV(estimator=lin_svm, step=1, cv=StratifiedKFold(label_readmission, 5),
 		              scoring='accuracy')
@@ -510,7 +510,7 @@ def main():
 		# plt.savefig("feature_selection/RFECV_bag_of_drugs_{flag}_{n}_featuresSet_C_{c}_optimal_{k}.png".format(flag = flag, n= len(feature_weights_bag_of_drugs[feature_weights_bag_of_drugs!=0]), c = bag_drugs_penalty, k = rfecv.n_features_))
 		# plt.show()
 		# plt.clf()
-	
+
 
 	# Compare the optimal accuracies; Notice that For Raw bag of drugs, the 2nd last and 3rd last run gives the best accuracy of 0.541271161176 by keeping the [ 1  8 11 12 14 17] or [0 1 8 11 12 14 15 17 ] drugs
 	# For normlized bag of drugs: keep 8 drugs in the feature set gives the best accuracy of 0.540101797498 by keeping  [ 0  1  6  9 10 11 15] in the set
@@ -552,22 +552,22 @@ def main():
 	# print chi_sq_selector.get_support(indices=True)
 	print np.argsort(chi_sq_selector.scores_)[::-1]
 	print np.sort(chi_sq_selector.scores_)[::-1]
-	
+
 	# Raw bag of drug selection is similar to that of l1 SVM method
 	print "chi_sq_selector on raw_bag_drugs" # ranking: [15  0  1  6 11  9 10 12 14  2 19  4  7 17  8  5 18 20 16 13  3]
 	chi_sq_selector = fs.SelectKBest(fs.chi2, k=10).fit(data_bag_drugs.toarray().astype(float), label_readmission)
 	# print chi_sq_selector.get_support(indices=True)
 	print np.argsort(chi_sq_selector.scores_)[::-1]
 	print np.sort(chi_sq_selector.scores_)[::-1]
-	
+
 	#---------------variable ranking: Single feature Knn performance------------
-	# Numerical, OrderedDict([(0.55329884698368959, [5]), (0.52759201386961918, [7]), (0.51347227480024271, [4]), (0.51190984770272352, [1]), (0.50994460160897082, [2]), (0.50538470063241681, [0]), (0.49359315016821775, [3]), (0.47728056901021559, [6])])	
+	# Numerical, OrderedDict([(0.55329884698368959, [5]), (0.52759201386961918, [7]), (0.51347227480024271, [4]), (0.51190984770272352, [1]), (0.50994460160897082, [2]), (0.50538470063241681, [0]), (0.49359315016821775, [3]), (0.47728056901021559, [6])])
 	# print np.unique(normalized_data_numerical[:,0])
 	print featureSelection_SingleVariablePerformance(data = normalized_data_numerical, label = label_readmission, analysis = "KNN")
-	
+
 	# Normalized Bag of Drugs, [12 | 17 | 20 18  5 19 | 16 | 0 | 1 | 11 | 4 | 2 | 15 | 14 | 8 | 13 | 9 | 6 | 7 | 10 | 3]
 	print featureSelection_SingleVariablePerformance(data = normalized_bag_drugs, label = label_readmission, analysis = "KNN")
-	
+
 	# Raw Bag of Drugs, same as normalized bag of drugs: OrderedDict([(0.53920759394477735, [12]), (0.5391486350604382, [17]), (0.53911915585958892, [5, 18, 19, 20]), (0.53865729637833237, [16]), (0.53381307351705176, [0]), (0.52489104452855595, [1]), (0.52381996866933833, [11]), (0.5232598546812286, [4]), (0.52277834159475201, [2]), (0.51957405803809131, [15]), (0.47659274593528222, [14]), (0.47655343824210672, [8]), (0.47652396097248345, [13]), (0.4637502429830197, [9]), (0.46333742167737402, [6]), (0.46262997203310513, [7]), (0.46257096245652402, [10]), (0.46086119198358694, [3])])
 	# print "Raw Bag of Drugs Single feature performance KNN"
 	# print featureSelection_SingleVariablePerformance(data = data_bag_drugs.toarray().astype(float), label = label_readmission, analysis = "KNN")
@@ -575,17 +575,17 @@ def main():
 	#---------------variable ranking: Single feature Linear SVM performance------------
 	# Numerical, [7 6 5  3/0 2 1 4], with [0,3] same performance ; OrderedDict([(0.59553309710983116, [7]), (0.56933579536811496, [6]), (0.55458617816804079, [5]), (0.53911915585958892, [0, 3]), (0.53793996755039686, [2]), (0.53692781533223133, [1]), (0.53172973101867327, [4])])
 	print featureSelection_SingleVariablePerformance(data = normalized_data_numerical, label = label_readmission, analysis = "SVM_Linear")
-	
+
 	# Normalized Bag of drugs, OrderedDict([(0.54060294390951324, [1]), (0.53968910751365728, [11]), (0.53921742098880254, [12]), (0.53911915585958892, [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 18, 19, 20]), (0.53910932978122417, [17])])
 	print featureSelection_SingleVariablePerformance(data = normalized_bag_drugs, label = label_readmission, analysis = "SVM_Linear")
-	
+
 	# Raw Bag of Durgs, OrderedDict([(0.54060294390951324, [1]), (0.53968910751365728, [11]), (0.53921742098880254, [12]), (0.5391486350604382, [17]), (0.53911915585958892, [0, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 18, 19, 20]), (0.5390208950754678, [3]), (0.53860816308952764, [2])])
 	# print "Raw bag of Drugs single feature performance SVM_Linear"
 	# print featureSelection_SingleVariablePerformance(data = data_bag_drugs.toarray().astype(float), label = label_readmission, analysis = "SVM_Linear")
 
 
 	#-----------------------------Run Analysis on Selected Features--------------------------------------
-	
+
 	# print "--------------Knn on the selected significant features-------------"
 	# numerical_selected = [7,6,5,1]
 	# KNearestNeighbour_Analysis(data = normalized_data_numerical[:,numerical_selected], label_column = label_readmission, num_folds = 5, num_neighbours = 7, weighted = False)
@@ -594,7 +594,7 @@ def main():
 
 	print "-------------Comparison of Analysis with and without Age factor-----------"
 	print normalized_data_numerical[:,1:].shape
-	
+
 	print "Knn with age"
 	KNearestNeighbour_Analysis(data = normalized_data_numerical, label_column = label_readmission, num_folds = 5, num_neighbours = 7, weighted = False)
 	print "Knn without age"
@@ -607,7 +607,7 @@ def main():
 	SVM_Analysis(data = normalized_data_numerical, label_column = label_readmission,num_folds = 5, SVMToUse = SVM_SVC)
 	print  "SVC SVM without age"
 	SVM_Analysis(data = normalized_data_numerical[:,1:], label_column = label_readmission,num_folds = 5, SVMToUse = SVM_SVC)
-	
+
 	data_categorical = loadArray("data_categorical.npz")
 	combined_data_numbers = np.concatenate((data_numerical , data_categorical), axis = 1)
 	print  "Decision Tree with age"
@@ -640,4 +640,4 @@ def main():
 
 if __name__ == "__main__":
 	main()
-# 
+#
